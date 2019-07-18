3c046520dbb657e96072db5eb4e5ece1f3331c75
maryam904 <mabdelrhman904@gmail.com>
2019-07-18 14:50:37 +0200

    fixing dashboard

---
+++ src/main/java/Exceptions/UserExceptions.java
@@ -0,0 +1,12 @@
+package Exceptions;
+
+@SuppressWarnings("serial")
+
+public class UserExceptions extends BaseException{
+
+	public UserExceptions(String description, int status) {
+		super(description, status);
+		// TODO Auto-generated constructor stub
+	}
+
+}
