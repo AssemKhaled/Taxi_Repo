dcf94cdf89ad5f6ee73fcb5408cec2950841cc03
fuinco <maryem@fuinco.com>
2022-02-15 15:22:18 +0200

    Adding new signatures

--- src/main/java/com/example/examplequerydslspringdatajpamaven/service/TaxiProfileService.java
+++ src/main/java/com/example/examplequerydslspringdatajpamaven/service/TaxiProfileService.java
@@ -6,7 +6,9 @@ public interface TaxiProfileService {
     ResponseEntity<?> getAllTaxiProfile(String TOKEN, Long loggerId, String name, int pageSize, int pageNumber);
     ResponseEntity<?> createTaxiProfile(String TOKEN, Long loggerId,TaxiProfileDto taxiProfileDto);
     ResponseEntity<?> editTaxiProfile(String TOKEN, Long loggerId, TaxiProfileDto taxiProfileDto);
-    ResponseEntity<?> deleteTaxiProfile(String TOKEN, Long loggerId, Long userId);
+    ResponseEntity<?> deleteTaxiProfile(String TOKEN, Long loggerId, Long taxiProfileId);
     ResponseEntity<?> GetUserChildren(String TOKEN, Long loggerId, Long UserId, int pageSize, int pageNumber);
+    ResponseEntity<?> getTaxiProfileById(String TOKEN, Long UserId, Long taxiProfileId);
+    ResponseEntity<?> assignTaxiProfileToDevice(String TOKEN, Long userId, Long taxiProfileId, Long deviceId);
 //    ResponseEntity<?> getTaxiProfileByName(String name);
 }
