3989475b652c99a37daf639a0182b1a3fbd72d26
fuinco <maryem@fuinco.com>
2022-02-15 15:21:31 +0200

    Adding rest controller annotation

--- src/main/java/com/example/examplequerydslspringdatajpamaven/rest/ElmConnectionsRestController.java
+++ src/main/java/com/example/examplequerydslspringdatajpamaven/rest/ElmConnectionsRestController.java
@@ -8,15 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.examplequerydslspringdatajpamaven.service.ElmServiceImpl;
 
@@ -26,9 +18,10 @@ import com.example.examplequerydslspringdatajpamaven.service.ElmServiceImpl;
  * @author fuinco
  *
  */
-@CrossOrigin
 //@Component
 @RequestMapping(path = "/elm")
+@CrossOrigin
+@RestController
 public class ElmConnectionsRestController {
 
 	
