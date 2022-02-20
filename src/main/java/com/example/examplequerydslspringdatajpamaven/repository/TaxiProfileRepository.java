c322ffa830752c60cbc78b5ffbfd7032751647a3
fuinco <maryem@fuinco.com>
2022-02-20 19:35:39 +0200

    Adding a method to get all the taxi profiles for a certain user or group of user

--- src/main/java/com/example/examplequerydslspringdatajpamaven/repository/TaxiProfileRepository.java
+++ src/main/java/com/example/examplequerydslspringdatajpamaven/repository/TaxiProfileRepository.java
@@ -1,8 +1,11 @@
 package com.example.examplequerydslspringdatajpamaven.repository;
 
 import com.example.examplequerydslspringdatajpamaven.entity.TaxiProfile;
+import com.example.examplequerydslspringdatajpamaven.responses.TaxiProfileListSelect;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.xml.crypto.Data;
@@ -16,4 +19,8 @@ public interface TaxiProfileRepository extends JpaRepository<TaxiProfile, Long> 
     List<TaxiProfile> findAllByCompanyIdIn(List<Long> childrenId);
     List<TaxiProfile> findAllByCompanyIdInAndDeleteDate(List<Long> childrenId, Data data, Pageable pageable);
     long countAllByName(String name);
+
+    @Query(value = "SELECT tc_profile_taxi.id,tc_profile_taxi.name FROM tc_profile_taxi"
+            + " WHERE tc_profile_taxi.companyId IN (:userIds ) and tc_profile_taxi.delete_date is null",nativeQuery = true)
+    List<TaxiProfileListSelect> getTaxiProfileByCompanyIds(@Param("userIds")List<Long> userIds);
 }
