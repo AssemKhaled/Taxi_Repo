3478e53d173ff562b2ecfc2a0cd4a0faf3456bb1
fuinco <maryem@fuinco.com>
2022-02-27 12:21:28 +0200

    Adding the id and name of the taxi profile fields response

--- src/main/java/com/example/examplequerydslspringdatajpamaven/responses/TaxiProfileListSelect.java
+++ src/main/java/com/example/examplequerydslspringdatajpamaven/responses/TaxiProfileListSelect.java
@@ -1,13 +1,7 @@
 package com.example.examplequerydslspringdatajpamaven.responses;
 
-import lombok.*;
 
-@AllArgsConstructor
-@Setter
-@Getter
-@NoArgsConstructor
-@Builder
-public class TaxiProfileListSelect {
-    private Long id;
-    private String name;
+public interface TaxiProfileListSelect {
+    Long getId();
+    String getName();
 }
