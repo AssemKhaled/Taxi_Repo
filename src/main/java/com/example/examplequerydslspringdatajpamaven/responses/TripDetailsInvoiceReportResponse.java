18393516d6253c284c8d543a2f6e64e9c1536c45
fuinco <maryem@fuinco.com>
2022-04-05 16:10:03 +0200

    Modifying the data type of the dates to strings

--- src/main/java/com/example/examplequerydslspringdatajpamaven/responses/TripDetailsInvoiceReportResponse.java
+++ src/main/java/com/example/examplequerydslspringdatajpamaven/responses/TripDetailsInvoiceReportResponse.java
@@ -1,5 +1,6 @@
 package com.example.examplequerydslspringdatajpamaven.responses;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.util.Date;
@@ -11,16 +12,19 @@ import java.util.Date;
 @Builder
 public class TripDetailsInvoiceReportResponse {
     private String tripId;
+    private Long driverId;
     private String driverName;
+    private String driverUniqueId;
     private String vehicleName;
-    private Date pickupDatetime;
-    private Date dropDateTime;
+    private String pickupDatetime;
+    private String dropDateTime;
     private String duration;
     private Double totalCost;
     private Double distance;
     private Integer paymentMethod;
     private Double totalWaitingCost;
     private Double totalDistanceCost;
+    private String totalWaitingTime;
     private Long basicCost;
     private Double totalVat;
     private Long actualCost;
