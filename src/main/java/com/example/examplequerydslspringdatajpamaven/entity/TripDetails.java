a76d81ec560c4f4b7042ac0d3d8c1bebdf819790
fuinco <maryem@fuinco.com>
2022-04-05 16:07:21 +0200

    Adding the query mapping and the filter column:

--- src/main/java/com/example/examplequerydslspringdatajpamaven/entity/TripDetails.java
+++ src/main/java/com/example/examplequerydslspringdatajpamaven/entity/TripDetails.java
@@ -1,10 +1,88 @@
 package com.example.examplequerydslspringdatajpamaven.entity;
 
 
+import com.example.examplequerydslspringdatajpamaven.responses.IncomeReportDetailsPerDay;
+import com.example.examplequerydslspringdatajpamaven.responses.IncomeReportDetailsPerDriverOrVehicle;
 import lombok.*;
 
 import javax.persistence.*;
 import java.util.Date;
+
+@SqlResultSetMappings({
+    @SqlResultSetMapping(
+        name="incomeReportDetailsPerDayMapping",
+        classes={
+            @ConstructorResult(
+                targetClass= IncomeReportDetailsPerDay.class,
+                columns={
+                    @ColumnResult(name="pickupDate",type=Date.class),
+                    @ColumnResult(name="filter",type=Date.class),
+                    @ColumnResult(name="totalIncome",type=Double.class),
+                    @ColumnResult(name="totalNumberOfTrips",type=int.class),
+                    @ColumnResult(name="TotalVat",type=Double.class),
+                    @ColumnResult(name="NetProfit",type=Double.class),
+                    @ColumnResult(name="TotalCash",type=Double.class),
+                    @ColumnResult(name="TotalCredit",type=Double.class),
+                }
+            ),
+        }
+    ),
+    @SqlResultSetMapping(
+        name="incomeReportDetailsPerVehicleOrDriverMapping",
+        classes={
+            @ConstructorResult(
+                targetClass= IncomeReportDetailsPerDriverOrVehicle.class,
+                columns={
+                    @ColumnResult(name="id",type=Long.class),
+                    @ColumnResult(name="name",type=String.class),
+                    @ColumnResult(name="filter",type=String.class),
+                    @ColumnResult(name="totalIncome",type=Double.class),
+                    @ColumnResult(name="totalNumberOfTrips",type=int.class),
+                    @ColumnResult(name="TotalVat",type=Double.class),
+                    @ColumnResult(name="NetProfit",type=Double.class),
+                    @ColumnResult(name="TotalCash",type=Double.class),
+                    @ColumnResult(name="TotalCredit",type=Double.class),
+                }
+            ),
+        }
+    ),
+})
+
+@NamedNativeQueries({
+    @NamedNativeQuery(name="incomeReportDetailsPerDay",
+        resultSetMapping="incomeReportDetailsPerDayMapping",
+        query="Select tc_trip_details.pickup_date AS pickupDate, tc_trip_details.pickup_date AS filter," +
+                "SUM(tc_trip_details.total_cost) AS totalIncome, COUNT(*) AS totalNumberOfTrips," +
+                "SUM(tc_trip_details.total_vat) AS totalVat, SUM(tc_trip_details.actual_cost) AS netProfit," +
+                "SUM(CASE WHEN tc_trip_details.payment_method = 0 THEN tc_trip_details.total_cost END ) totalCash," +
+                "SUM(CASE WHEN tc_trip_details.payment_method = 1 THEN tc_trip_details.total_cost END ) totalCredit" +
+                " FROM tc_trip_details Where tc_trip_details.driver_id IN(:driverIds)" +
+                "and tc_trip_details.pickup_date_time BETWEEN :start AND :end GROUP BY tc_trip_details.pickup_date LIMIT :offset,10"
+    ),
+    @NamedNativeQuery(name="incomeReportDetailsPerDriver",
+        resultSetMapping="incomeReportDetailsPerVehicleOrDriverMapping",
+        query="Select tc_trip_details.driver_id AS id , tc_drivers.name AS name, tc_drivers.name AS filter," +
+                " SUM(tc_trip_details.total_cost) AS totalIncome, COUNT(*) AS totalNumberOfTrips," +
+                " SUM(tc_trip_details.total_vat) AS totalVat, SUM(tc_trip_details.actual_cost) AS NetProfit," +
+                " SUM(CASE WHEN tc_trip_details.payment_method = 0 THEN tc_trip_details.total_cost END ) totalCash," +
+                " SUM(CASE WHEN tc_trip_details.payment_method = 1 THEN tc_trip_details.total_cost END ) totalCredit" +
+                " FROM tc_trip_details  INNER Join tc_drivers ON tc_trip_details.driver_id = tc_drivers.id" +
+                " Where tc_trip_details.driver_id IN(:driverIds)" +
+                " and tc_trip_details.pickup_date_time BETWEEN :start AND :end GROUP BY tc_trip_details.driver_id LIMIT :offset,10"
+    ),
+    @NamedNativeQuery(name="incomeReportDetailsPerDevice",
+        resultSetMapping="incomeReportDetailsPerVehicleOrDriverMapping",
+        query="Select tc_devices.id AS id , tc_devices.name AS name, tc_devices.name AS filter," +
+                " SUM(tc_trip_details.total_cost) AS totalIncome, COUNT(*) AS totalNumberOfTrips," +
+                " SUM(tc_trip_details.total_vat) AS totalVat, SUM(tc_trip_details.actual_cost) AS NetProfit," +
+                " SUM(CASE WHEN tc_trip_details.payment_method = 0 THEN tc_trip_details.total_cost END ) totalCash," +
+                " SUM(CASE WHEN tc_trip_details.payment_method = 1 THEN tc_trip_details.total_cost END ) totalCredit" +
+                " FROM tc_trip_details  INNER Join tc_devices ON tc_trip_details.driver_id = tc_devices.driverId" +
+                " INNER Join tc_device_driver ON tc_trip_details.driver_id = tc_device_driver.driverid" +
+                " Where tc_trip_details.driver_id IN(:driverIds)" +
+                " and tc_trip_details.pickup_date_time BETWEEN :start AND :end GROUP BY tc_devices.id LIMIT :offset,10"
+    )
+})
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -105,4 +183,5 @@ public class TripDetails {
     private Long actualCost;
 
 }
+
 
